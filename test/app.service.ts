c94f7838da3ce0a58fafcabd8eca8228edeeaf7e
蔡德森 <asen001@live.com>
2022-04-08 13:31:16 +0800

    test: core functions test

--- test/app.service.ts
+++ test/app.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { Cacheable } from '../src';
+import { CacheEvict } from '../src';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
+  wait(ms: number) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  @Cacheable({
+    key: (id: number) => `username-${id}`,
+    namespace: 'user',
+  })
+  async getUserName(id: number) {
+    await this.wait(100);
+    return `xyz`;
+  }
+
+  @CacheEvict({
+    key: (id: number) => `username-${id}`,
+    namespace: 'user',
+  })
+  async resetUserInfo(id: number) {
+    return true;
   }
 }
---
+++ test/app.spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppService } from './app.service';
+import { CacheableModule } from '../src';
+import { CACHE_MANAGER, CacheModule } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+
+describe('App', () => {
+  let appService: AppService;
+  let cacheManager: Cache;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        CacheableModule.register(),
+        CacheModule.register({ isGlobal: true }),
+      ],
+      providers: [AppService],
+    }).compile();
+    await app.createNestApplication().init();
+    appService = app.get<AppService>(AppService);
+    cacheManager = app.get<Cache>(CACHE_MANAGER);
+  });
+
+  describe('main', () => {
+    it('cache eq return', async () => {
+      const res = await appService.getUserName(1);
+      const cachedValue = await cacheManager.get('user:username-1');
+      expect(res).toBe(cachedValue);
+    });
+    it('cache evict', async () => {
+      const res = await appService.getUserName(1);
+      const cachedValue = await cacheManager.get('user:username-1');
+      expect(res).toBe(cachedValue);
+      await appService.resetUserInfo(1);
+      expect(await cacheManager.get('user:username-1')).toBeUndefined();
+    });
+  });
+});
